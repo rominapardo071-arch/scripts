4c71d30b284d2176a9879e949be7560f46013247
rominapardo071-arch <rominapardo071@gmail.com>
2026-07-04 18:47:05 -0300

    Add files via upload
    usuarios con cifrado

--- usuarios.sh
+++ usuarios.sh
@@ -1,46 +1,172 @@
 #!/bin/bash
-clear
-#verificamos si existe el fichero logs.txt
-verif_logs=$(ls logs.txt 2>/nev/null)
-if [ -z "$verif_logs" ]; then
-touch logs.txt
-#si no existe lo crea > logs.txt
-fi
+clave= "1234"
+LOG="Log_usuarios.enc"
+cifrar(){
+echo "$1" | openssl enc -aes-256-cbc -a -salt -pbkdf2 -pass pass:$clave >> "$LOG"
+}
 menu(){
-echo "MENU DE USUARIOS"
-echo "1- agregar usuario"
-echo "2- borrar usuario"
-echo "3- modificar usuario"
-echo "4- lista usuarios"
+clear
+echo "Menú Gestión de Usuarios: "
+echo ""
+echo "1- Agregar Usuario"
+echo "2- Borrar Usuario"
+echo "3- Modificar Usuario"
+echo "4- Lista de Usuarios"
 echo "5- crear grupo"
 echo "6- listar grupo"
-echo "7- salir"
-read opc
+echo "7- eliminar grupo"
+echo "8- Salir"
+echo "*) opcion incorrecta"
+read -p "Seleccione opción: " opc
 case $opc in
+
 1) agregarUser;;
 2) borrarUser;;
-3) modificarUser;;
-4) listaUser;;
-5) creagrupo;;
-6) listagrupo;;
-7) salir;;
+3) modUser;;
+4) ListaUsuarios;;
+5) crearGrupo;;
+6) listarGrupo;;
+7) borrarGrupo;;
+8) salir;;
 esac
 }
 
 agregarUser(){
-clear
- read -p "ingrese nombre" us
- read -p "ingrese grupo al que pertenece" gr
-read -p "ingrese contraseña" ps
-useradd -d/home/$us-s/bin/bash -g $gr -m $ps
-echo "usuario creado correctamente"
-sleep 2
-echo "creando password para usuario"
-echo $us $ps | chpasswd
-sleep 2
-clear
-echo "usuario creado con exito"
-sleep 3
+
+read -p "Ingrese Nuevo Usuario:" user
+sudo useradd -m -d "/home/$user" -s /bin/bash "$user"
+if [ $? -eq 0 ]; then
+sudo passwd "$user"
+echo "$user creado correctamente"
+cifrar "usuario creado: $user"
+else
+echo "error al crear usuario"
+fi
+sleep 4
+menu
+}
+borrarUser(){
+echo "introduce el nombre de usuario que deseas eliminar"
+read -p "usuario: " user
+sudo userdel -r $user
+if [ $? -eq 0 ]; then
+echo "usuario $user se elimino correctamente"
+cifrar "usuario eliminado: $user"
+else
+echo "error el usuario no existe"
+fi
+sleep 4
 menu
 }
 
+modUser(){
+echo "MODIFICAR USUARIO"
+echo "1) cambiar nombre"
+echo "2) modificar contraseña"
+echo "3) cambiar grupo"
+
+read -p "opcion:" op
+
+case $op in
+1) cambiarnombre ;;
+2) modPasswd ;;
+3) cambiagrupo ;;
+esac
+}
+
+cambiarnombre(){
+read -p "ingrese usuario a modificar:" usuario
+if id "$usuario" &>/dev/null; then
+read -p "ingrese nuevo nombre:" nombre
+sudo usermod -l "$nombre" "$usuario"
+echo "usuario modificado correctamente"
+else
+echo "el usuario no existe"
+fi
+sleep 4
+menu
+}
+modPasswd(){
+read -p "ingrese el nombre del usuario:" usuario
+if id "$usuario" &>/dev/null; then
+sudo passwd "$usuario"
+echo "se ha modificado la contraseña"
+cifrar "contraseña para: $usuario"
+else
+echo "usuario no encontrado"
+fi
+sleep 4
+menu
+}
+
+cambiagrupo(){
+read -p "usuario:" usuario
+read -p "grupo:" grupo
+ if id "$usuario" &>/dev/null; then
+        sudo usermod -aG "$grupo" "$usuario"
+        echo "Grupo modificado correctamente"
+    else
+        echo "Usuario no encontrado"
+    fi
+sleep 4
+menu
+}
+ListaUsuarios(){
+clear
+echo "usuarios:"
+echo ""
+awk -F: '$3 >= 1000 {print $1}' /etc/passwd
+echo ""
+read -p "presione enter para volver atras" b
+sleep 2
+menu
+}
+crearGrupo(){
+read -p "ingrese nombre de grupo a crear:" grupo
+grupo=$(echo "$grupo" | xargs)
+
+echo "DEBUG creando: '$grupo'"
+
+sudo groupadd $grupo
+code=$?
+
+echo "DEBUG exit code: $code"
+if [ $code -eq 0 ]; then
+echo "$grupo" >> grupos_creados.txt
+echo "grupo creado correctamente"
+else
+echo "error al crear grupo"
+fi
+sleep 2
+menu
+}
+listarGrupo(){
+clear
+echo "aqui estan los grupos"
+cat grupos_creados.txt
+read -p "precione enter para volver" b
+sleep 3
+menu
+}
+borrarGrupo(){
+echo "ingrese nombre de grupo a eliminar"
+read gr
+gr=$(echo "$gr" | xargs)
+echo "DEBUG: '$gr'"
+sudo groupdel "$gr"
+
+if [ $? -eq 0 ]; then
+sed -i "/^$gr$/d" grupos_creados.txt
+echo "grupo eliminado correctamente"
+else
+echo "el grupo no existe"
+fi
+sleep 4
+menu
+}
+salir(){
+echo "saliendo...."
+sleep 3
+exit
+}
+menu
